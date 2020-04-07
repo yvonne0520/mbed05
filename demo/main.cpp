e3ee2a3e847042e6ae44956b4c936de63f39f4ab
yvonne0520 <yvonne8805200520@gmail.com>
2020-04-07 14:10:42 +0800

    demo1

---
+++ demo/main.cpp
@@ -0,0 +1,15 @@
+#include "mbed.h"
+
+DigitalOut led3(LED3);
+EventQueue queue(32*EVENTS_EVENT_SIZE);
+
+void blue_led()
+{
+    led3 = !led3;
+}
+
+int main()
+{
+    queue.cal_every(500, blue_led);
+    queue.dispatch();
+}
